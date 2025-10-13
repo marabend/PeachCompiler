37c7ac57c6a08b4db385bd6ed81dc64b97621646
compiler <dev.marcel.abend@gmail.com>
2025-10-13 15:30:48 +0200

    Skeleton for compiler

---
+++ main.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+#include "helpers/vector.h"
+#include "compiler.h"
+
+int main()
+{
+    int res = compile_file("./test.c", "./test", 0);
+    if (res == COMPILER_FILE_COMPILED_OK)
+    {
+        printf("Everything compiled just fine\n");
+    }
+    else if( res == COMPILER_FAILED_WITH_ERRORS)
+    {
+        printf("Compile failed\n");
+    }
+    else
+    {
+        printf("Unknown response for compile file\n");
+    }
+
+    return 0;
+}
